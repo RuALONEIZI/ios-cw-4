97823b2dbe23bb8ca7a460afa97cb1d295bba310
RuALONEIZI <100702655+RuALONEIZI@users.noreply.github.com>
2022-03-07 20:58:08 +0300

    done

---
+++ cw 2-4.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+import Foundation
+
+var greeting = "Hello, playground"
+
+var name = " ruaa "
+
+var favouriteNumber = 3
+
+var hobby = " table tennis "
+
+print( السلام عليكم، معاكم ورقمي المفضل هو واحب     hobby favouriteNumber Name )
+
+
+var height = 1.54
+
+var age = 17 
